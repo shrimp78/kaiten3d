db0edfcd1332f6193df3830c1fbedc6f21068b6d
shrimp78 <y.kitamura78@gmail.com>
2015-09-15 19:28:08 +0900

    ignore swap files

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,15 @@
 class TopController < ApplicationController
   def index
   end
+
+  def upload
+    file = params[:img]
+    name = file[:datafile].original_filename
+    @file_path = "/img/#{name}"
+    File.open("public#{@file_path}", 'wb') { |f|
+      f.write(file[:datafile].read)
+    }
+
+    render 'index'
+  end
 end
