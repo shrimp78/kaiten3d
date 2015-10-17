5a58c0e82e15a0297199716e934663717168bccf
kitamura <y.kitamura78@gmail.com>
2015-10-17 16:09:11 +0900

    画像意外のものがアップ去れた時のエラーハンドリング

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,10 +3,21 @@ class TopController < ApplicationController
   end
 
   def upload
-    file = params[:img][:datafile]
-    name = file.original_filename
-    @file_path = "/img/#{name}"
-    @full_path = "public#{@file_path}"
+    if file = params[:img]
+      file = file[:datafile]
+      name = file.original_filename
+      ext_name = File.extname(name)
+      unless ext_name =~ /[jpeg|jpg|png]/
+        @msg = "なんやねん、そのファイル"
+        render 'error' and return
+      end
+      @file_path = "/img/#{name}"
+      @full_path = "public#{@file_path}"
+      @msg = "はい、できました"
+    else
+      @msg = "画像ファイルきてないで"
+      render 'error' and return
+    end
 
     # 保存のタイミング
     File.open(@full_path, 'wb') { |f| f.write(file.read) }
